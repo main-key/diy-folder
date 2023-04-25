a0fa0622db0216cf9de6374646892e683673f75e
抓到一个小丑 <64778656+main-key@users.noreply.github.com>
2023-04-25 17:18:04 +0800

    提交源码
    bat打包之前源码

---
+++ md.bat
@@ -0,0 +1,83 @@
+@echo off & color F1 & set "info=创建DIY文件夹"
+setlocal enabledelayedexpansion
+set "pathCd=%cd%"
+:main
+set "fileName="
+set "LocalizedResourceName="
+set "IconResource="
+set "InfoTip="
+set "to="
+cls
+title [ %info% ] -按提示输入信息(1/3)
+set "t1=	" & set "t2=		"
+echo. & echo [ 当前路径 ]%cd%
+echo. & echo [ 在当前路径DIY一个文件夹，支持自定义参数为 ]
+echo.
+echo %t1%1、别名
+echo %t1%2、显示图标（ico）格式
+echo %t1%3、文件夹备注
+
+:: 录入名称
+echo. & echo. & set /p "fileName=[ 键入文件夹名称 ] "
+if "%fileName%" equ "" ( goto main )
+
+:: 录入其他信息
+echo. & echo. & echo [ 分别输入‘文件夹别名’‘文件夹图标(ico)格式’‘文件夹备注’留空则忽略 ] 选填
+echo. & set /p "LocalizedResourceName=[ 键入文件夹 别名 ] "
+echo. & set /p "IconResource=[ 键入文件夹 图标 ] "
+echo. & set /p "InfoTip=[ 键入文件夹 备注 ] "
+echo.
+if "%LocalizedResourceName%" equ "" (
+	if "%IconResource%" equ "" (
+		if "%InfoTip%" equ "" (
+			echo [ 其他信息为空，巧妇难为无米之炊 ] - 再见！
+			pause>nul
+			exit /b
+		)
+	)
+)
+cls
+title [ %info% ] - 检查并确认(2/3)
+echo. & echo [ 检查以下输入信息 ]
+echo. 
+echo [ 目标 ] %pathCd%\%fileName%
+set "to=%pathCd%\%fileName%"
+if "%LocalizedResourceName%" neq "" (
+	echo. & echo [ 别名 ] %LocalizedResourceName%
+)
+if "%IconResource%" neq "" (
+	echo. & echo [ 图标 ] %IconResource%
+)
+if "%InfoTip%" neq "" (
+	echo. & echo [ 备注 ] %InfoTip%
+)
+echo. & echo. & choice /c:01 /n /m:"[ 确认无误后键入‘1’开始，键入‘0’返回主菜单 ] "
+set "ChoiceParam=%errorlevel%"
+if "%ChoiceParam%" equ "1" goto main
+if "%ChoiceParam%" equ "2" goto ste
+pause>nul & exit /b
+
+:ste
+:: 进入目录位置，创建所需文件，加上属性
+md "%fileName%"
+if "%LocalizedResourceName%" neq "" (
+	:: echo LocalizedResourceName=%LocalizedResourceName%>>Desktop.ini
+	DeskEdit.exe /F="%to%" /S=.ShellClassInfo /L=LocalizedResourceName=%LocalizedResourceName%
+)
+if "%IconResource%" neq "" (
+	:: echo IconResource=%IconResource%^,0 >>Desktop.ini
+	DeskEdit.exe /F="%to%" /S=.ShellClassInfo /L=IconResource=%IconResource%
+)
+if "%InfoTip%" neq "" (
+	:: echo InfoTip=%InfoTip%>>Desktop.ini
+	DeskEdit.exe /F="%to%" /S=.ShellClassInfo /L=InfoTip=%InfoTip%
+)
+cls
+cd /d %pathCd%
+title [ %info% ] - 处理完成(3/3)
+echo. & echo. & echo Quotations:
+echo %t1%我的微笑，你百度都搜不到。
+echo %t1%生来骄傲，不爽绕道。
+echo %t1%你是限量版，但我是绝版。
+echo %t1%生活处处是阳光，看你自己怎么想。
+echo. & echo. & echo [ 按任意键重复。。。 ] & pause > nul & goto main
